8614bc171e23486ed72b6a40fe1996daf9e2af24
Tomi <equivalent@eq8.eu>
2014-01-08 09:18:12 +0000

    Create rvm.rb

---
+++ rvm.rb
@@ -0,0 +1,20 @@
+
+
+## Upgrade rvm 
+
+     rvm get stable
+
+## install ruby
+
+     rvm install ruby-2.1.0
+     
+     
+## gemset & ruby version 
+
+
+    # ./.ruby-version
+    2.1
+    
+    # ./.ruby-gemset
+    my_app_gemset_name
+    
